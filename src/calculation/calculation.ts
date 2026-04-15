cc2b8337979f58d32c23932ca9cb3ae98c2653ca
moritz <moritzmaier353@gmail.com>
2026-04-15 13:57:45 +0200

    Komponente Für den Rechenweg

---
+++ src/calculation/calculation.ts
@@ -0,0 +1,28 @@
+import { Component,OnInit,Input,Output,EventEmitter,ElementRef,ViewChild,AfterViewInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calculation',
+  imports: [],
+  templateUrl: './calculation.html',
+  styleUrl: './calculation.css',
+})
+export class Calculation implements OnInit,AfterViewInit {
+  @Input("data") data: any;
+  @ViewChild("container") container!: ElementRef;
+  @Output() close = new EventEmitter<void>;
+
+  public showFullCalc: boolean = false;
+
+  onToggleCalculation() {
+    this.close.emit();
+  }
+  constructor() {}
+
+  ngOnInit(): void {
+    
+    
+  }
+  ngAfterViewInit() {
+
+  }
+}
